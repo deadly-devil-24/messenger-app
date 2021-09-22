3f211d22a9c85f69c879a2864d0292994673fe31
Prabodh <64888445+deadly-devil-24@users.noreply.github.com>
2021-09-22 13:58:42 +0530

    messenger updated

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+const request = require('request');
 // Imports dependencies and set up http server
 const
   express = require('express'),
@@ -18,12 +21,25 @@ app.post('/webhook', (req, res) => {
   
       // Iterates over each entry - there may be multiple if batched
       body.entry.forEach(function(entry) {
+
+        // Gets the body of the webhook event
+    let webhook_event = entry.messaging[0];
+    console.log(webhook_event);
   
-        // Gets the message. entry.messaging is an array, but 
-        // will only ever contain one message, so we get index 0
-        let webhook_event = entry.messaging[0];
-        console.log(webhook_event);
-      });
+  
+    // Get the sender PSID
+    let sender_psid = webhook_event.sender.id;
+    console.log('Sender PSID: ' + sender_psid);
+  
+    // Check if the event is a message or postback and
+    // pass the event to the appropriate handler function
+    if (webhook_event.message) {
+      handleMessage(sender_psid, webhook_event.message);        
+    } else if (webhook_event.postback) {
+      handlePostback(sender_psid, webhook_event.postback);
+    }
+    
+  });
   
       // Returns a '200 OK' response to all requests
       res.status(200).send('EVENT_RECEIVED');
@@ -60,3 +76,58 @@ app.get('/webhook', (req, res) => {
       }
     }
   });
+  //Handles messages events
+  function handleMessage(sender_psid, received_message) {
+
+    let response;
+  
+    // Checks if the message contains text
+    if (received_message.text) {
+      
+      // Creates the payload for a basic text message, which
+      // will be added to the body of our request to the Send API
+      response = {
+        "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
+      }
+  
+    } else if (received_message.attachments) {
+    
+      // Gets the URL of the message attachment
+      let attachment_url = received_message.attachments[0].payload.url;
+    
+    } 
+    
+    // Sends the response message
+    callSendAPI(sender_psid, response);    
+  }
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+// Construct the message body
+let request_body = {
+  "recipient": {
+    "id": sender_psid
+  },
+  "message": response
+}
+
+// Send the HTTP request to the Messenger Platform
+request({
+  "uri": "https://graph.facebook.com/v2.6/me/messages",
+  "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
+  "method": "POST",
+  "json": request_body
+}, (err, res, body) => {
+  if (!err) {
+    console.log('message sent!')
+  } else {
+    console.error("Unable to send message:" + err);
+  }
+}); 
+}
